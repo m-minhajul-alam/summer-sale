c0c32fe6874a5a2d3832b11ee25e582de43267d9
Minhajul-alam <alam.minhajul@outlook.com>
2023-08-21 23:17:33 +0600

    resize font, bug fix

--- js/summer-sale.js
+++ js/summer-sale.js
@@ -4,50 +4,40 @@ function clickedCard(card) {
     const selectedItems = document.getElementById('selected-items');
     const count = selectedItems.childElementCount;
     const itemName = card.childNodes[3].childNodes[3].innerText;
-
     const li = document.createElement('li');
     li.innerHTML = `${count + 1}. ${itemName}`;
-
     selectedItems.appendChild(li);
     li.classList.add('my-3')
-
     const itemPrice = card.childNodes[3].childNodes[5].innerText.split(" ")[0];
-
     total = total + parseFloat(itemPrice);
     const totalInDecimal = total.toFixed(2);
-
     document.getElementById('total-price').innerText = totalInDecimal;
     document.getElementById('total').innerText = totalInDecimal;
-
     if (totalInDecimal > 0) {
         document.getElementById('purchase-btn').removeAttribute("disabled");
         if (totalInDecimal >= 200) {
+            document.getElementById('input-field').removeAttribute("disabled");
             document.getElementById('coupon-btn').removeAttribute("disabled");
 
         }
     }
 }
 
-
 function calculateDiscount(totalPrice) {
-
     const doscount = ((totalPrice * 20) / 100);
     const totalDoscount = doscount.toFixed(2);
     document.getElementById('discount').innerText = totalDoscount;
-
     const discountPrice = totalPrice - doscount;
     const totalDiscountPrice = discountPrice.toFixed(2);
     document.getElementById('total').innerText = totalDiscountPrice;
-
-
 }
 
 document.getElementById('coupon-btn').addEventListener('click', function () {
     const inputField = document.getElementById('input-field');
     const inputValue = inputField.value;
-    // console.log(inputValue);
     if (inputValue !== 'SELL200') {
-        alert('coupon is not valid')
+        alert('Coupon is not valid. Please, provide a valid coupon.')
+        document.getElementById('input-field').value = "";
         return;
     } else {
         const totalPriceString = document.getElementById('total-price').innerText;
